8abc7e9419fe73c4975e8d6a8db44f03056d5e31
otumian <popecan1000@gmail.com>
2024-10-04 04:50:47 +0000

    create some transactions and sign them with the key pair generated

--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,125 @@
-import Block from "./block";
 import BlockChain from "./block_chain";
 import Transaction from "./transaction";
-import { Log } from "./util";
+import { GENESIS_WALLET, GenerateKeyPair, Log } from "./util";
+
+// generate get the private key
+const myKeyPair = GenerateKeyPair();
+const myWalletAddress = myKeyPair.getPublic("hex");
+
+const yourKeyPair = GenerateKeyPair();
+const yourWalletAddress = yourKeyPair.getPublic("hex");
 
 const uCoin = new BlockChain();
 
-uCoin.createTransaction(new Transaction("genesisAddress1", "user1Address", 9));
-uCoin.createTransaction(new Transaction("genesisAddress1", "user2Address", 3));
-uCoin.createTransaction(new Transaction("user1Address", "user2Address", 5));
+uCoin.addTransaction(
+    new Transaction(GENESIS_WALLET.ADDR_1, myWalletAddress, 9)
+);
+uCoin.addTransaction(
+    new Transaction(GENESIS_WALLET.ADDR_1, yourWalletAddress, 10)
+);
 
-uCoin.minePendingTransactions("user1Address");
+/* Log({
+    log: `genesisAddress0 balance: ${uCoin.getBalanceOfAddress(GENESIS_WALLET.ADDR_1)}`,
+});
+Log({
+    log: `genesisAddress1 balance: ${uCoin.getBalanceOfAddress(GENESIS_WALLET.ADDR_1)}`,
+});
+Log({
+    log: `myBalance balance: ${uCoin.getBalanceOfAddress(myWalletAddress)}`,
+});
+Log({
+    log: `yourBalance balance: ${uCoin.getBalanceOfAddress(yourWalletAddress)}`,
+});
+ */
 
-// Log({ "log": `genesisAddress0 balance: ${uCoin.getBalanceOfAddress("genesisAddress0")}` });
-// Log({ "log": `genesisAddress1 balance: ${uCoin.getBalanceOfAddress("genesisAddress1")}` });
-// Log({ "log": `user1Address balance: ${uCoin.getBalanceOfAddress("user1Address")}` });
-// Log({ "log": `user2Address balance: ${uCoin.getBalanceOfAddress("user2Address")}` });
+uCoin.minePendingTransactions(myWalletAddress);
+const trx = new Transaction(myWalletAddress, yourWalletAddress, 3);
+trx.signTransaction(myKeyPair);
+uCoin.addTransaction(trx);
+// Log(uCoin);
 
-uCoin.minePendingTransactions("genesisAddress0");
-uCoin.createTransaction(new Transaction("user1Address", "user2Address", 2));
-uCoin.minePendingTransactions("genesisAddress0");
+/* Log({
+    log: `genesisAddress0 balance: ${uCoin.getBalanceOfAddress(GENESIS_WALLET.ADDR_1)}`,
+});
+Log({
+    log: `genesisAddress1 balance: ${uCoin.getBalanceOfAddress(GENESIS_WALLET.ADDR_1)}`,
+});
+Log({
+    log: `myBalance balance: ${uCoin.getBalanceOfAddress(myWalletAddress)}`,
+});
+Log({
+    log: `yourBalance balance: ${uCoin.getBalanceOfAddress(yourWalletAddress)}`,
+}); */
+// uCoin.addTransaction(new Transaction(myWalletAddress, yourWalletAddress, 2));
+uCoin.minePendingTransactions(myWalletAddress);
 
-
-Log({ "log": `genesisAddress0 balance: ${uCoin.getBalanceOfAddress("genesisAddress0")}` });
-Log({ "log": `genesisAddress1 balance: ${uCoin.getBalanceOfAddress("genesisAddress1")}` });
-Log({ "log": `user1Address balance: ${uCoin.getBalanceOfAddress("user1Address")}` });
-Log({ "log": `user2Address balance: ${uCoin.getBalanceOfAddress("user2Address")}` });
+/* Log({
+    log: `genesisAddress0 balance: ${uCoin.getBalanceOfAddress(GENESIS_WALLET.ADDR_1)}`,
+});
+Log({
+    log: `genesisAddress1 balance: ${uCoin.getBalanceOfAddress(GENESIS_WALLET.ADDR_1)}`,
+});
+Log({
+    log: `myBalance balance: ${uCoin.getBalanceOfAddress(myWalletAddress)}`,
+});
+Log({
+    log: `yourBalance balance: ${uCoin.getBalanceOfAddress(yourWalletAddress)}`,
+}); */
 
 // uCoin.addBlock(
 //     new Block(new Date("2024-10-02T07:45:06.903Z"), [
 
-//         new Transaction("genesisAddress1", "user2Address", 10),
+//         new Transaction(GENESIS_WALLET.ADDR_1, yourWalletAddress, 10),
 //     ])
 // );
 
 // uCoin.addBlock(
 //     new Block(new Date("2024-10-02T08:45:06.903Z"), [
-//         new Transaction("user1Address", "user2Address", 2.5),
-//         // new Transaction("genesisAddress1", "user2Address", 10),
+//         new Transaction(myWalletAddress, yourWalletAddress, 2.5),
+//         // new Transaction(GENESIS_WALLET.ADDR_1, yourWalletAddress, 10),
 //     ])
 // );
 
 // Log({ log: `uCoin chain is ${uCoin.isChainValid() ? "valid" : "invalid"}` });
 // Log(uCoin);
-// uCoin._chain[2]._data = { message: "huhu" };
-// uCoin._chain[2]._hash = uCoin._chain[2].calculateHash();
+uCoin.minePendingTransactions(myWalletAddress);
+// add invalid transaction
+
+/* 
+const invalidTransaction = new Transaction(
+    GENESIS_WALLET.ADDR_1,
+    myWalletAddress,
+    57
+)
+uCoin._chain[1].transactions[0] = invalidTransaction
+// invalidTransaction
+//     .signTransaction(myKeyPair);
+
+uCoin._chain[1]._hash = uCoin._chain[1].calculateHash();
+ */
+
+/* uCoin._chain[1].transactions[0]._amount = 20;
+// invalidTransaction
+//     .signTransaction(myKeyPair);
+
+uCoin._chain[1]._hash = uCoin._chain[1].calculateHash();
+ */
+Log({ log: `uCoin chain is ${uCoin.isChainValid() ? "valid" : "invalid"}` });
 
 // Log({ log: `uCoin chain is ${uCoin.isChainValid() ? "valid" : "invalid"}` });
+// Log(uCoin);
+
+// Log({
+//     log: `genesisAddress0 balance: ${uCoin.getBalanceOfAddress(GENESIS_WALLET.ADDR_1)}`,
+// });
+// Log({
+//     log: `genesisAddress1 balance: ${uCoin.getBalanceOfAddress(GENESIS_WALLET.ADDR_1)}`,
+// });
+// Log({
+//     log: `user1Address balance: ${uCoin.getBalanceOfAddress(myWalletAddress)}`,
+// });
+// Log({
+//     log: `user2Address balance: ${uCoin.getBalanceOfAddress(yourWalletAddress)}`,
+// });
+
 Log(uCoin);
-
-
-Log({ "log": `genesisAddress0 balance: ${uCoin.getBalanceOfAddress("genesisAddress0")}` });
-Log({ "log": `genesisAddress1 balance: ${uCoin.getBalanceOfAddress("genesisAddress1")}` });
-Log({ "log": `user1Address balance: ${uCoin.getBalanceOfAddress("user1Address")}` });
-Log({ "log": `user2Address balance: ${uCoin.getBalanceOfAddress("user2Address")}` });
